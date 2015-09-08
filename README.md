e0d1bc1b52760cd2e0589a9cde5a29b777835e95
Andrey Sitnik <andrey@sitnik.ru>
2015-09-08 21:27:29 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Show Box [![Travis Build Status][ci-img]][ci]
+
+[ci-img]: https://travis-ci.org/ai/showbox.svg
+[ci]:     https://travis-ci.org/ai/showbox
---
+++ gulpfile.babel.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp';
+
+gulp.task('clean', () => {
+    let del = require('del');
+    return del(['build/']);
+});
+
+// Build
+
+gulp.task('build:lib', ['clean'], () => {
+    let babel = require('gulp-babel');
+    return gulp.src('lib/*.es6')
+        .pipe(babel())
+        .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('build:docs', ['clean'], () => {
+    let ignore = require('fs').readFileSync('.npmignore').toString().trim()
+        .split(/\n+/)
+        .concat(['.npmignore'])
+        .map( i => '!' + i );
+    return gulp.src(['*'].concat(ignore))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['build:lib', 'build:docs']);
+
+// Lint
+
+gulp.task('lint', () => {
+    let eslint = require('gulp-eslint');
+    return gulp.src(['*.js', 'lib/*.es6', 'test/*.es6'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+// Tests
+
+gulp.task('test', () => {
+});
+
+// Common
+
+gulp.task('default', ['lint', 'test']);
